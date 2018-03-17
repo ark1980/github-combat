a165dba7b6a582e359566e960b031026bee1881d
Ali <ar.k@outlook.com>
2018-03-17 15:41:36 -0700

    create Navbar component and add some css for styling

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <header className="App-header">
+      <div className="App-logo">
+        <h2>GITHUB COMBAT</h2>
+      </div>
+      <nav className="navbar">
+        <ul>
+          <li>HOME</li>
+          <li>BATTLE</li>
+          <li>POPULAR</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar;
